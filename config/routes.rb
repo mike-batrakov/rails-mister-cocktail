0e136f6268d67e368dd56fc29bc4611201ef3e69
Michael Batrakov <mykhailobatrakov@gmail.com>
2021-02-20 15:40:33 -0500

    Add controllers and actions

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'cocktails#index'
   resources :cocktails, only: %i[index create new show] do
-    resources :doses, only: %i[index]
+    resources :doses, only: %i[create]
   end
+  resources :doses, only: %i[destroy]
 end
